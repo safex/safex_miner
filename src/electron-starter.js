ca2ec4d44cf203dbbabbe8f5f78da765ba5115cd
Uros <umitrovic1@gmail.com>
2018-09-26 15:36:30 +0200

    setup Win installer

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,3 +1,10 @@
+//handle setupevents as quickly as possible
+const setupEvents = require('./setupWinEvents');
+if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
+
 const electron = require('electron');
 // Module to control application life.
 const {app, Menu} = require('electron');
