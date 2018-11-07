c030183c80d9ccb13909a92151f472e7a7d2e608
Uros <umitrovic1@gmail.com>
2018-11-07 11:43:12 +0100

    set safexnews pool on low end hardware port

--- src/components/MiningApp.js
+++ src/components/MiningApp.js
@@ -325,7 +325,7 @@ export default class MiningApp extends React.Component {
                         </div>
 
                         <select className="pool-url" name="pool" id="pool" disabled={this.state.active ? "disabled" : ""}>
-                            <option>pool.safexnews.net:3333</option>
+                            <option>pool.safexnews.net:1111</option>
                             <option>safex.cnpools.space:3333</option>
                             <option>safex.cryptominingpools.net:3333</option>
                             <option>safex.luckypool.io:3366</option>
