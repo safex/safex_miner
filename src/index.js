44d1402835b653c3130a5fbb380347941f3e08b2
Uros <umitrovic1@gmail.com>
2019-01-20 23:53:14 +0100

    remove routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
-import routes from './routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import MiningApp from "./components/MiningApp";
 
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(<MiningApp />, document.getElementById('root'));
